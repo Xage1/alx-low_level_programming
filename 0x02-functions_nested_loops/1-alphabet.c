186962eb0694e5c04ca70d86228a22a9bd898ea4
Xage1 <kennethkiarie555@gmail.com>
2023-03-22 05:38:26 -0700

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,8 @@
  */
 void print_alphabet(void)
 {
-char c = 'a';
-
-while (c <= 'z')
-{
-_putchar(c);
-c++;
-}
-
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 _putchar('\n');
 }
-
