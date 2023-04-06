f983fd687f7db90a602a648e87e79a37135e5be7
Xage1 <kennethkiarie555@gmail.com>
2023-04-06 11:25:07 -0700

    Recursion project ALX

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - check the function in them main.h header file.
+ * _print_rev_recursion - check the function in the main.h header file.
  * @s: character of a string
  * Return: Always 0.
  */
@@ -10,8 +10,9 @@ void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
 {
+return;
+}
 _print_rev_recursion(s + 1);
 _putchar(*s);
-}
 }
 
