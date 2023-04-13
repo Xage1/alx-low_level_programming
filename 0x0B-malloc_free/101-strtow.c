1cc87d2d8ba5966e51d05d6d1454b9a6d6d16524
Xage1 <kennethkiarie555@gmail.com>
2023-04-13 12:37:51 -0700

    C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,102 +1,66 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * count_words - function to count the number of words in a string
- * @str: The input string
- * Return: Always 0
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
  */
-int count_words(char *str)
+int count_word(char *s)
 {
-int count = 0;
-int in_word = 0;
-int i;
-for (i = 0; str[i] != '\0'; i++)
+int flag, c, w;
+flag = 0;
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
 {
-if (str[i] != ' ')
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
 {
-if (in_word == 0)
-{
-count++;
-in_word = 1;
+flag = 1;
+w++;
 }
 }
-else
-{
-in_word = 0;
+return (w);
 }
-}
-return (count);
-}
-
 /**
- * copy_word - function to copy a word from a string to a new buffer
- * @str: The input string
- * Return: Always 0
- */
-char *copy_word(char *str)
-{
-char *word;
-int  len = 0;
-int i = 0;
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\0')
-{
-len++;
-}
-word = (char *) malloc((len + 1) * sizeof(char));
-if (word == NULL)
-{
-return (NULL);
-}
-strncpy(word, str, len);
-word[len] = '\0';
-return (word);
-}
-
-/**
- * strtow - function that splits a string into words.
- * @str: A given Array
- * Return: Always 0
- */
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings (Success) or NULL (Error)
+*/
 char **strtow(char *str)
 {
-char **words;
-int num_words, i, j;
-char *word;
-if (str == NULL || *str == '\0')
-{
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
 return (NULL);
-}
-num_words = count_words(str);
-words = (char **) malloc((num_words + 1) * sizeof(char *));
-if (words == NULL)
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
 return (NULL);
-i = 0;
-while (*str != '\0')
+for (i = 0; i <= len; i++)
 {
-if (*str == ' ' || *str == '\t' || *str == '\n')
+if (str[i] == ' ' || str[i] == '\0')
 {
-str++;
-}
-else
+if (c)
 {
-word = copy_word(str);
-if (word == NULL)
-{
-for (j = 0; j < i; j++)
-{
-free(words[j]);
-}
-free(words);
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
 return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
 }
-words[i] = word;
-i++;
-str += strlen(word);
 }
+else if (c++ == 0)
+start = i;
 }
-words[num_words] = NULL;
-return (words);
+matrix[k] = NULL;
+return (matrix);
 }
