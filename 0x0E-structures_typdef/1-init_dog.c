63b79dc150b2485448ff01a895bbe04fb1e78b7c
Xage1 <kennethkiarie555@gmai.com>
2023-04-17 06:43:16 -0700

    C-Structures and Typedef

--- 0x0E-structures_typdef/1-init_dog.c
+++ 0x0E-structures_typdef/1-init_dog.c
@@ -7,25 +7,14 @@
  * @age: Age of the dog
  * @owner: Owner of the dog
  * @d: Pointer in the structure dog
+ * Return: Always 0.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
 {
 d->name = name;
 d->age = age;
 d->owner = owner;
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-struct dog my_dog;
-init_dog(&my_dog, "Poppy", 3.5, "Bob");
-
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
 }
