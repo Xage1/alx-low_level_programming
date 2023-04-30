37ca152c6cf8d07148c41ffc4e4652b6670dafd6
Xage1 <kennethkiarie555@gmail.com>
2023-04-30 04:43:56 -0700

    Singly linked lists Poject

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * __attribute__((constructor)) print_chat - prints a message 
+ * Return: Void
+ */
+
+void __attribute__((constructor)) print_chat()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
